1fa1ea05e803a986b2e556497c09a722c63f4f46
Yuri <yuri.yakovlev@skitlab.ru>
2015-04-15 15:18:13 +0400

    Добавил возможность загружать CSV файлы из шаблона документа, таким образом появилась возможность создавать один документ из множества CSV файлов

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from jinja2 import Environment, PackageLoader, FileSystemLoader, BaseLoader
+import jinja2
+from jinja2 import Environment, PackageLoader, FileSystemLoader, BaseLoader, nodes
+from jinja2.ext import Extension
 import csv
 import sys
 import logging
@@ -12,21 +14,34 @@ def UnicodeDictReader(utf8_data, **kwargs):
 	for row in csv_reader:
 		yield dict([(key, unicode(value, 'utf-8')) for key, value in row.iteritems()])
 
+
+@jinja2.contextfunction
+def load_csv(context, arg):
+	logging.info("Read CSV (%s)" % arg)
+	csvfile = open(arg, 'rt')
+	csvreader = UnicodeDictReader(csvfile)
+	csvdata = list(csvreader)
+	csvfile.close()
+	logging.info("Read %d lines" % len(csvdata))
+	return csvdata
+
 def main(options):
 	logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
 	logging.info("Read template")
 	env = Environment(loader=FileSystemLoader(".", encoding='utf-8'))
 	template = env.get_template(options.template)
 
-	logging.info("Read CSV")
-	csvfile = open(options.csv, 'rt')
-	csvreader = UnicodeDictReader(csvfile)
-	csvdata = list(csvreader)
-	csvfile.close()
-	logging.info("Read %d lines" % len(csvdata))
+	env.globals.update(load_csv = load_csv)
+
+	#logging.info("Read CSV")
+	#csvfile = open(options.csv, 'rt')
+	#csvreader = UnicodeDictReader(csvfile)
+	#csvdata = list(csvreader)
+	#csvfile.close()
+	#logging.info("Read %d lines" % len(csvdata))
 
 	logging.info("Template rendering...")
-	output_data = template.render( csv=csvdata )
+	output_data = template.render( )
 
 	logging.info("Template rendering done")
 
@@ -39,15 +54,15 @@ def main(options):
 parser = OptionParser(version='0.1', description='Python programm to processing template')
 #ls.add_param("-l", "--long", help="list in long format", default=False, action="store_true")
 parser.add_option("-t", "--template", help="File with template", type="string")
-parser.add_option("-c", "--csv", help="File with data in CSV format", type="string")
+#parser.add_option("-c", "--csv", help="File with data in CSV format", type="string")
 parser.add_option("-o", "--output", help="File to save data", type="string")
 
 
 if __name__ == "__main__":
 	parser.print_help()
 	(options, args) = parser.parse_args()
-	if (not options.template) or (not options.csv) or (not options.output):
-		parser.error("set options -t, -c and -o")
+	if (not options.template) or (not options.output):
+		parser.error("set options -t and -o")
 
 	main(options)
 
